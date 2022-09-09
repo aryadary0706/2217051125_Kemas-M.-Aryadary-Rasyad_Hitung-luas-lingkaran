4cd16694c6a96a9953bd8bc3f1e678ddcb99e755
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2022-09-09 12:38:45 +0700

    Add files via upload

---
+++ 2217051125_Kemas M. Aryadary Rasyad_hitung luas lingkaran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+		float luas, pi=3.14;
+		int r;
+		cout<<"menghitung luas lingkaran";
+		cout<<endl;
+		cout<<"masukkan jari-jari";
+		cin>>r;
+		luas=pi*r*r;
+		cout<<endl;
+		cout<<"luas lingkatan : "<<luas;
+		
+		return 0;
+		
+}
